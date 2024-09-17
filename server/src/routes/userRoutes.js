ffa1a3c0d8f3e08e639e0d755c2238561a264974
Rajveer Singh1 <rajveer.singh1@celebaltech.com>
2024-09-17 21:44:55 +0530

    fixed api

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -15,7 +15,7 @@ router.get('/user/exists/:id',userController.isExist)
 router.get('/user/verifyemail/:token',userController.verifyEmail)
 router.get('/user/get-user-data', authenticateToken,userController.getUserData);
 router.get('/user/get-referral-data', authenticateToken,userController.getUserReferrals);
-router.get('/user/fetch-user-data/:email',adminValidation,authenticateToken,userController.fetchUser); //admin route
+router.get('/user/fetch-user-data/:email',authenticateToken,adminValidation,userController.fetchUser); //admin route
 router.get('/user/get-total-investment',authenticateToken,adminValidation, userController.getTotalInvestment); //admin route
 router.get('/user/get-total-users',authenticateToken,adminValidation,userController.getTotalUsers); //admin route
 router.get('/user/get-analytics',authenticateToken,adminValidation,userController.getAnalytics); //admin route
